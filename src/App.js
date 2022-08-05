be9da7e069d9b94c9ce7a3774259b7a68bd50cd9
cafeorea <cafeorea@users.noreply.github.com>
2022-08-05 20:02:38 +0900

    Output dynamic list data with map method

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
+import React, { useState } from "react";
 import Expense from "./components/Expenses/Expense";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Car Insurance",
+    amount: 140,
+    date: new Date(2021, 12, 28),
+  },
+  {
+    id: "e2",
+    title: "Rent",
+    amount: 600,
+    date: new Date(2022, 4, 19),
+  },
+  {
+    id: "e3",
+    title: "Macbook Pro",
+    amount: 1200,
+    date: new Date(2022, 6, 12),
+  },
+  {
+    id: "e4",
+    title: "Return Flight Tickets (Mexico)",
+    amount: 850,
+    date: new Date(2022, 7, 20),
+  },
+];
+
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Car Insurance",
-      amount: 140,
-      date: new Date(2021, 12, 28),
-    },
-    {
-      id: "e2",
-      title: "Rent",
-      amount: 600,
-      date: new Date(2022, 4, 19),
-    },
-    {
-      id: "e3",
-      title: "Macbook Pro",
-      amount: 1200,
-      date: new Date(2022, 6, 12),
-    },
-    {
-      id: "e4",
-      title: "Return Flight Tickets (Mexico)",
-      amount: 850,
-      date: new Date(2022, 7, 20),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
   return (
     <div>
